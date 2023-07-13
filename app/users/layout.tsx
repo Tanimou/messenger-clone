ab1677a3584f145370d17ebbb28c63987c39c69d
Tanimou Cisse <tanimou@live.fr>
2023-07-13 09:31:44 +0000

    Sidebar navigation+Layout

---
+++ app/users/layout.tsx
@@ -0,0 +1,14 @@
+import SideBar from "../components/sidebar/SideBar"
+
+export default async function UsersLayout({
+    children,
+}: {
+    children: React.ReactNode
+}) {
+    return (
+        
+        <SideBar>
+            <div className="h-full">{children}</div>
+        </SideBar>
+    )
+}
